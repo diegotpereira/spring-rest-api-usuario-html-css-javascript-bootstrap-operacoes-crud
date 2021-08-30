fda581155947282ae932248deaf3c79afa7492fa
Diego <diegoteixeirapereira@hotmail.com>
2021-08-30 14:47:26 -0300

    first commit

---
+++ src/main/java/br/com/java/springrestapiusuario/SpringrestapiusuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.springrestapiusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestapiusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestapiusuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/springrestapiusuario/SpringrestapiusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.springrestapiusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestapiusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
